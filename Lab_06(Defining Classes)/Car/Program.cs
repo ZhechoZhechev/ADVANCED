2f974c894338269a322b4e3e5303a2ab6160d6a7
ZhechoZhechev <mishomishev024@gmail.com>
2022-10-03 19:06:25 +0300

    Defining Classes Lab

---
+++ Lab_06(Defining Classes)/Car/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Tire[]> tirePacks = new List<Tire[]>();
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+            string input;
+            while ((input = Console.ReadLine()) != "No more tires")
+            {
+                string[] tireYearAndPressure = input.Split();
+                Tire[] singleTires = new Tire[4];
+                int indexCounter = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    int tireYear = int.Parse(tireYearAndPressure[indexCounter]);
+                    indexCounter++;
+                    double pressureValue = double.Parse(tireYearAndPressure[indexCounter]);
+                    singleTires[i] = new Tire(tireYear, pressureValue);
+                    indexCounter++;
+                }
+                tirePacks.Add(singleTires);
+            }
+
+            while ((input = Console.ReadLine()) != "Engines done")
+            {
+                string[] engineInfo = input.Split();
+                int horsePower = int.Parse(engineInfo[0]);
+                double cubicCapacity = double.Parse(engineInfo[1]);
+                engines.Add(new Engine(horsePower, cubicCapacity));
+            }
+
+            while ((input = Console.ReadLine()) != "Show special")
+            {
+                string[] carInfo = input.Split();
+                string make = carInfo[0];
+                string model = carInfo[1];
+                int year = int.Parse(carInfo[2]);
+                double fuelQuantity = double.Parse(carInfo[3]);
+                double fuelConsumption = double.Parse(carInfo[4]);
+                int engineIndex = int.Parse(carInfo[5]);
+                int tiresIndex = int.Parse(carInfo[6]);
+
+                Tire[] currentCarTires = tirePacks[tiresIndex];
+                Engine currentCarEngine = engines[engineIndex];
+                cars.Add(new Car(make, model, year, fuelQuantity, fuelConsumption, currentCarEngine, currentCarTires));
+            }
+
+            List<Car> specialCars = cars
+                .FindAll(c => c.Year >= 2017
+                           && c.Engine.HorsePower > 330
+                           && c.Tires.Select(t => t.Preassure).Sum() >= 9
+                           && c.Tires.Select(t => t.Preassure).Sum() <= 10);
+
+            foreach (Car car in specialCars)
+            {
+                car.Drive(20);
+                Console.WriteLine(car);
+            }
+
+        }
+    }
+}
---
+++ Lab_06(Defining Classes)/Car/Tire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public int Year { get { return year; } set { year = value; } }
+        public double Preassure { get { return pressure; } set { pressure = value; } }
+
+        public Tire(int year, double preassure)
+        {
+            this.Year = year;
+            this.Preassure = preassure;
+        }
+    }
+}
